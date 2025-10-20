6e3174d2da32c9f0eb927ff69263bae38db0ccec
DazKins <david.atkins1998@gmail.com>
2025-10-20 22:17:51 +0800

    add dev mode

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io/fs"
 	"log"
+	"os"
 	"webscape/server"
 )
 
@@ -12,12 +13,24 @@ import (
 var DistFS embed.FS
 
 func main() {
+	dev := flag.Bool("dev", false, "Use live filesystem instead of embedded files")
 	flag.Parse()
+
 	var f fs.FS
 	var err error
-	f, err = fs.Sub(DistFS, "client/dist")
-	if err != nil {
-		log.Fatal(err)
+
+	if *dev {
+		// Development mode: read from actual filesystem
+		log.Println("Using live filesystem (development mode)")
+		f = os.DirFS("client/dist")
+	} else {
+		// Production mode: use embedded files
+		log.Println("Using embedded filesystem (production mode)")
+		f, err = fs.Sub(DistFS, "client/dist")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
 	server.Start(f)
 }
